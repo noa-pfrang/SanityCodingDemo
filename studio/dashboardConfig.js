5adbae0803fc25f329a78119f0e0410f23cc7e07
Peder Frang <peder.frang@noaignite.com>
2022-03-08 12:38:09 +0100

    first

--- studio/dashboardConfig.js
+++ studio/dashboardConfig.js
@@ -1,8 +1,9 @@
 export default {
   widgets: [
-   
+
     {name: 'structure-menu'},
     {name: 'project-users', layout: {height: 'auto', widht:'medium'}},
+    {name: 'cats', layout: {width: 'full'}},
     {
       name: 'document-list',
       options: {title: 'Recently edited', order: '_updatedAt desc', limit: 10, types: ['post']},
